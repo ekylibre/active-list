a5ba35bd765a5e08466d7caaa43a6db129ef705c
Brice Texier <burisu@oneiros.fr>
2013-02-19 08:26:37 +0100

    Changes record use

--- lib/active-list/exporters/excel_csv_exporter.rb
+++ lib/active-list/exporters/excel_csv_exporter.rb
@@ -19,10 +19,10 @@ module ActiveList
       record = "r"
       code  = table.select_data_code(:paginate => false)
       encoding = "CP1252"
-      code << "data = ActiveList::CSV.generate(:col_sep=>';') do |csv|\n"
+      code << "data = ActiveList::CSV.generate(:col_sep => ';') do |csv|\n"
       code << "  csv << [#{columns_to_array(table, :header, :encoding => '#{encoding}').join(', ')}]\n"
       code << "  for #{record} in #{table.records_variable_name}\n"  
-      code << "    csv << [#{columns_to_array(table, :body, :record=>record, :encoding => '#{encoding}').join(', ')}]\n"
+      code << "    csv << [#{columns_to_array(table, :body, :record => #{record}, :encoding => '#{encoding}').join(', ')}]\n"
       code << "  end\n"
       code << "end\n"
       code << "send_data(data, :type => #{self.mime_type.to_s.inspect}, :disposition => 'inline', :filename => #{table.model.name}.model_name.human.gsub(/[^a-z0-9]/i,'_')+'.#{self.file_extension}')\n"
